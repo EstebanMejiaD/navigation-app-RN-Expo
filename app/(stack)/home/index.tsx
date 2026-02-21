81a8041f381546e76f5947ca9d570aac2adb8c29
Esteban Mejia <mejiaesteban965@gmail.com>
2026-02-20 20:41:14 -0500

    aplicacion basica con stack router

---
+++ app/(stack)/home/index.tsx
@@ -0,0 +1,50 @@
+import CustomButton from "@/components/shared/CustomButton";
+import { Link, router } from "expo-router";
+import React from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View className="px-10 mt-5">
+        <CustomButton
+          className="mb-2"
+          color="primary"
+          onPress={() => router.push("/products")}
+        >
+          Productos
+        </CustomButton>
+
+        <CustomButton
+          className="mb-2"
+          color="secondary"
+          onPress={() => router.push("/profile")}
+        >
+          Perfil
+        </CustomButton>
+
+        <CustomButton
+          className="mb-2"
+          color="tertiary"
+          onPress={() => router.push("/settings")}
+        >
+          Ajustes
+        </CustomButton>
+
+        <Link href="/products" asChild>
+          <CustomButton
+            variant="text-only"
+            className="mb-10"
+            color="primary"
+            onPress={() => router.push("/products")}
+          >
+            Productos
+          </CustomButton>
+        </Link>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,34 @@
+import { Redirect } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View className="mt-6 mx-2.5">
-        <Text className="text-5xl" style={{ fontFamily: "WorkSans-Black" }}>
-          Hola mundo
-        </Text>
+  return <Redirect href="/(stack)/home" />;
 
-        <Text className="text-4xl font-work-black text-primary">
-          Hola mundo
-        </Text>
+  // return (
+  //   <SafeAreaView>
+  //     <View className="mt-6 mx-2.5">
+  //       <Text className="text-5xl" style={{ fontFamily: "WorkSans-Black" }}>
+  //         Hola mundo
+  //       </Text>
 
-        <Text className="text-3xl font-work-medium text-secondary">
-          Hola mundo
-        </Text>
+  //       <Text className="text-4xl font-work-black text-primary">
+  //         Hola mundo
+  //       </Text>
 
-        <Text className="text-2xl font-work-light text-secondary-100">
-          Hola mundo
-        </Text>
+  //       <Text className="text-3xl font-work-medium text-secondary">
+  //         Hola mundo
+  //       </Text>
 
-        <Text className="text-xl text text-tertiary">Hola mundo</Text>
-      </View>
-    </SafeAreaView>
-  );
+  //       <Text className="text-2xl font-work-light text-secondary-100">
+  //         Hola mundo
+  //       </Text>
+
+  //       <Text className="text-xl text text-tertiary">Hola mundo</Text>
+
+  //       <Link href="/products">Productos</Link>
+  //     </View>
+  //   </SafeAreaView>
+  // );
 };
 
 export default App;
