bb692837c9943fd138e12418ba0b7ab4b6e5f3c1
Esteban Mejia <mejiaesteban965@gmail.com>
2026-02-24 11:07:15 -0500

    agregada tipo de router tabs

--- app/index.tsx
+++ app/index.tsx
@@ -2,33 +2,8 @@ import { Redirect } from "expo-router";
 import React from "react";
 
 const App = () => {
-  return <Redirect href="/(stack)/home" />;
-
-  // return (
-  //   <SafeAreaView>
-  //     <View className="mt-6 mx-2.5">
-  //       <Text className="text-5xl" style={{ fontFamily: "WorkSans-Black" }}>
-  //         Hola mundo
-  //       </Text>
-
-  //       <Text className="text-4xl font-work-black text-primary">
-  //         Hola mundo
-  //       </Text>
-
-  //       <Text className="text-3xl font-work-medium text-secondary">
-  //         Hola mundo
-  //       </Text>
-
-  //       <Text className="text-2xl font-work-light text-secondary-100">
-  //         Hola mundo
-  //       </Text>
-
-  //       <Text className="text-xl text text-tertiary">Hola mundo</Text>
-
-  //       <Link href="/products">Productos</Link>
-  //     </View>
-  //   </SafeAreaView>
-  // );
+  // return <Redirect href="/(stack)/home" />;
+  return <Redirect href="/tabs/home" />;
 };
 
 export default App;
